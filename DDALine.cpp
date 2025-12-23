11d38b44296ae9135ded55f9e012b999f3e8470c
Saurav Khanal <sauravkhanal61@gmail.com>
2025-12-23 15:22:46 +0545

    updated lab 1

---
+++ DDALine.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<graphics.h>
+#include<conio.h>
+#include<math.h>
+using namespace std;
+int main(){
+    int k;
+    int gd=DETECT,gm;
+    float steps,x,y,x1,y1,x2,y2,dy,dx,delx,dely;
+    cout<<"Enter the value of x1, y1, x2, y2: ";
+    cin>>x1>>y1>>x2>>y2;
+    dy=y2-y1;
+    dx=x2-x1;
+    initgraph(&gd, &gm, (char*)"");
+    if(abs(dy)<abs(dx)){
+        steps=abs(dx);
+    }else{
+        steps=abs(dy);
+    }
+    delx=dx/steps;
+    dely=dy/steps;
+    x=x1;
+    y=y1;
+    putpixel(round(x),round(y),WHITE);
+    for(k=0;k<steps;k++){
+        x=x+delx;
+        y=y+dely;
+        putpixel(round(x),round(y),WHITE);
+    }
+    getch();
+    closegraph();
+}
