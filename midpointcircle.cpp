5a3e5938cd1d05d495ddc913011c4aed06fe3f1b
Saurav Khanal <sauravkhanal61@gmail.com>
2025-12-24 21:24:09 +0545

    midpoint circle drawing algorithm

---
+++ midpointcircle.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<graphics.h>
+#include<conio.h>
+#include<math.h>
+using namespace std;
+int main(){
+    int gd=DETECT, gm;
+    int xc, yc, r;
+    cout << "Enter the center coordinates (xc, yc) and radius r of the circle: ";
+    cin >> xc >> yc >> r;
+    initgraph(&gd, &gm, (char*)"");
+    int x = 0;
+    int y = r;
+    int Pk = 1 - r;
+    putpixel(xc + x, yc + y, WHITE);
+    putpixel(xc - x, yc + y, WHITE);
+    putpixel(xc + x, yc - y, WHITE);
+    putpixel(xc - x, yc - y, WHITE);
+    putpixel(xc + y, yc + x, WHITE);
+    putpixel(xc - y, yc + x, WHITE);
+    putpixel(xc + y, yc - x, WHITE);
+    putpixel(xc - y, yc - x, WHITE);
+    while(x<y)
+    {
+        x++;
+        if(Pk<0)
+        {
+            Pk = Pk + 2*x + 1;
+        }
+        else
+        {
+            y--;
+            Pk = Pk +2*x+1-2*y;
+        }
+        putpixel(xc + x, yc + y, WHITE);
+        putpixel(xc - x, yc + y, WHITE);
+        putpixel(xc + x, yc - y, WHITE);
+        putpixel(xc - x, yc - y, WHITE);
+        putpixel(xc + y, yc + x, WHITE);
+        putpixel(xc - y, yc + x, WHITE);
+        putpixel(xc + y, yc - x, WHITE);
+        putpixel(xc - y, yc - x, WHITE);
+    }
+    getch();
+    closegraph();
+    return 0;
+
+}
